4b1c851bee603fef2fe50fa391be0224ab1cd8e3
William Harris <willmharris0@gmail.com>
2020-01-26 21:02:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
   array.any? { |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(a_o_s)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
