46d5a273bc2117697b991f91468ac980f62b648f
William Harris <willmharris0@gmail.com>
2020-01-26 20:55:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index| 
+    puts "#{index + 1}. #{dwarf}"
+  end 
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
