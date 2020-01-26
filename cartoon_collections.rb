d2b22be3ea8a7927c5bc5cccb833b79bd3620c6e
William Harris <willmharris0@gmail.com>
2020-01-26 21:08:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.detect do |item| 
+  array.detect do |string| 
+    if item == "cheddar" 
   end 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
